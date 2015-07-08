df4e93dd7671e3f72930c6ee73055e7d297a27c2
Daniel Shukuya <shdaniel@gmail.com>
2015-07-07 22:45:28 -0300

    + avances

--- web/js/grayscale.js
+++ web/js/grayscale.js
@@ -38,7 +38,7 @@ function init() {
   // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
   var mapOptions = {
     // How zoomed in you want the map to start at (always required)
-    zoom: 15,
+    zoom: 13,
 
     // The latitude and longitude to center the map (always required)
     // center: new google.maps.LatLng(40.6700, -73.9400), // New York
@@ -128,11 +128,11 @@ function init() {
   var map = new google.maps.Map(mapElement, mapOptions);
 
   // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-  var image = 'img/map-marker.png';
+//  var image = 'img/map-marker2.png';
   var myLatLng = new google.maps.LatLng(-34.859509, -58.594090);
   var beachMarker = new google.maps.Marker({
     position: myLatLng,
     map: map,
-    icon: image
+//    icon: image
   });
 }
